0e1ac3c01d3124fb89f661364b71688585ca12fc
TusharSinghal90 <100600776+TusharSinghal90@users.noreply.github.com>
2026-02-01 18:10:01 +0530

    Remove CrossOrigin annotation from FlightController

--- src/main/java/com/venture/flight_booking/controllers/FlightController.java
+++ src/main/java/com/venture/flight_booking/controllers/FlightController.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-@CrossOrigin(origins = "*") 
+ 
 @RestController
 @RequestMapping("/api/flights")
 public class FlightController {
